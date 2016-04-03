2cf01e2980c91f59f28021aa5ab3dadef83afa70
huzheng001 <huzheng001@gmail.com>
2016-04-03 14:33:33 +0800

    0.5 version released!

---
+++ source_codes/stardict.org/ajax_win_default.php
@@ -0,0 +1,8 @@
+<?php
+	setcookie("StarDict_win_x", 155, time()+60*60*24*30, '/');
+	setcookie("StarDict_win_y", 70, time()+60*60*24*30, '/');
+	setcookie("StarDict_win_w", 600, time()+60*60*24*30, '/');
+	setcookie("StarDict_win_h", 450, time()+60*60*24*30, '/');
+
+	echo "Done!";
+?>
